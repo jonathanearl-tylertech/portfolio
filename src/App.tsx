596d81f83403860a2c0a4cdae9e402e4d4eb2f78
jonathan earl <earl.jonathan@gmail.com>
2022-12-24 01:54:43 -0800

    add projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes, A } from '@solidjs/router';
 import { Component, createSignal, For } from 'solid-js';
+import { FaSolidDiagramProject, FaSolidHouseChimney, FaSolidUserNinja } from "solid-icons/fa";
 import Home from './Home';
 import Project from './Project';
-import { FaSolidDiagramProject, FaSolidHouseChimney, FaSolidUserNinja } from "solid-icons/fa";
 
-const App: Component = () => {
+const App: Component = () => {  
   const [links] = createSignal([
     // { name: 'home', href: '/', icon: FaSolidHouseChimney },
     { name: 'projects', href: '/projects', icon: FaSolidDiagramProject },
@@ -12,13 +12,13 @@ const App: Component = () => {
   ]);
 
   return (
-    <div class="flex h-full">
+    <div class="flex h-full relative">
       <div class="flex flex-col w-[245px] px-[12px] border-black-200 border-r-[1px]">
         <div class="py-[30px] px-[12px] text-[29px]">
           Jonathan Earl
         </div>
         <nav class="flex flex-col gap-2">
-          <For each={links()}>{(link, i) =>
+          <For each={links()}>{(link) =>
             <A class="flex capitalize p-[12px] my-[8px]" href={link.href}>
               <link.icon class="mr-[16px]" size={24} />
               <span class="leading-[24px] text-[16px]">{link.name}</span>
--- src/Project.tsx
+++ src/Project.tsx
@@ -1,10 +1,65 @@
-import type { Component } from 'solid-js';
+import { A } from '@solidjs/router';
+import { Component, createSignal, For, Show } from 'solid-js';
+import dog from './assets/dog.jpg';
+import { FaSolidX, FaSolidArrowRight, FaSolidArrowLeft } from "solid-icons/fa";
 
-
+// import Porfolio from './projects/Porfolio.mdx';
 
 const Project: Component = () => {
+  const [projects] = createSignal([
+    { name: 'portfolio1', tags: ['terraform', 'javascript', 'front-end'], image: dog, },
+    { name: 'portfolio2', tags: ['terraform', 'javascript', 'front-end'], image: dog, },
+    { name: 'portfolio3', tags: ['terraform', 'javascript', 'front-end'], image: dog, },
+  ]);
+  const [count] = createSignal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  const [selectedProject, setSelectedProject] = createSignal(0)
+  const [show, setShow] = createSignal(false);
+
   return (
-    <div>PROJECT PAGE PLACEHOLDER</div>
+    <div class="mx-auto">
+      <Show when={show()}>
+        <div class="absolute flex justify-center items-center w-screen h-screen left-0 top-0 bg-black opacity-90"></div>
+        <div class="absolute flex justify-center items-center w-screen h-screen left-0 top-0" onclick={() => setShow(false)}>
+          <div class="flex opacity-100">
+            <img class="max-w-[764px] h-[764px] object-contain bg-black" src={projects()[selectedProject()].image} />
+            <div class="w-[500px] bg-white">
+              <header>
+                <h1>{projects()[selectedProject()].name}</h1>
+              </header>
+            </div>
+          </div>
+        </div>
+        <div class="absolute flex justify-center items-center w-screen h-screen left-0 top-0  ">
+          <button
+            class="absolute right-[24px] top-[24px] bg-white rounded-full cursor-pointer p-2"
+            onclick={() => setShow(false)}
+          >
+            <FaSolidX />
+          </button>
+          <button
+            class="absolute right-[24px] bg-white rounded-full p-2 cursor-pointer z-50"
+            onclick={(e) => { e.preventDefault(); setSelectedProject((selectedProject() + 1) % 3) }}
+          >
+            <FaSolidArrowRight />
+          </button>
+          <button
+            class="absolute left-[24px] bg-white rounded-full p-2 cursor-pointer z-50"
+            onclick={() => setSelectedProject((selectedProject() + 3 - 1) % 3)}
+          >
+            <FaSolidArrowLeft />
+          </button>
+        </div>
+      </Show>
+      <div class="text-[18px] leading-[24px] font-bold mt-[16px] mb-[24px] px-[20px]">Projects</div>
+      <div class="px-[20px] w-[935px] grid grid-cols-3 gap-[28px]">
+        <For each={count()}>{(i) =>
+          <img src={projects()[i % 3].image} onclick={() => {
+            setSelectedProject(i % 3);
+            setShow(true);
+          }} />
+        }</For>
+      </div>
+    </div >
   );
 };
 
