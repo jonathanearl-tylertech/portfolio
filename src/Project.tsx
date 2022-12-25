5c2ed1e7bd342ed3ca3451bbb44b003ce3206cbb
jonathan earl <earl.jonathan@gmail.com>
2022-12-25 11:15:14 -0800

    fix: click off modal to close

--- src/Project.tsx
+++ src/Project.tsx
@@ -44,7 +44,10 @@ const Project: Component = () => {
     setCurrentIndex(index);
   }
 
-  const deselectProject = () => {
+  const deselectProject = (e: MouseEvent) => {
+    const modal = document.querySelector('#modal');
+    if (modal && e.composedPath().includes(modal))
+      return;
     setSelectedProject(null);
   }
 
@@ -52,8 +55,14 @@ const Project: Component = () => {
     <div class="mx-auto">
       <Show when={selectedProject()}>
         <div class="absolute flex justify-center items-center w-screen h-screen left-0 top-0 bg-black opacity-90"></div>
-        <div class="absolute flex justify-center items-center w-screen h-screen left-0 top-0" onclick={deselectProject}>
-          <div class="flex opacity-100">
+        <div
+          class="absolute flex justify-center items-center w-screen h-screen left-0 top-0"
+          onclick={deselectProject}
+        >
+          <div
+            id="modal" 
+            class="flex"
+          >
             <img class="max-w-[764px] h-[764px] object-contain bg-black" src={dog} />
             <div class="w-[500px] bg-white">
               <header>
@@ -63,35 +72,35 @@ const Project: Component = () => {
             </div>
           </div>
         </div>
-        <div class="absolute flex justify-center items-center w-screen h-screen left-0 top-0  ">
+        <button
+          class="absolute top-[24px] right-[24px] bg-white rounded-full cursor-pointer p-2"
+          onclick={deselectProject}
+        >
+          <FaSolidX />
+        </button>
+        <Show when={currentIndex() !== projectList().length - 1}>
           <button
-            class="absolute right-[24px] top-[24px] bg-white rounded-full cursor-pointer p-2"
-            onclick={deselectProject}
+            id="prev-button"
+            class="absolute right-[24px] top-1/2 translate-y-1/2  bg-white rounded-full p-2 cursor-pointer z-50"
+            onclick={selectNextProject}
           >
-            <FaSolidX />
+            <FaSolidArrowRight />
           </button>
-          <Show when={currentIndex() !== projectList().length - 1}>
-            <button
-              class="absolute right-[24px] bg-white rounded-full p-2 cursor-pointer z-50"
-              onclick={selectNextProject}
-            >
-              <FaSolidArrowRight />
-            </button>
-          </Show>
-          <Show when={currentIndex() !== 0}>
-            <button
-              class="absolute left-[24px] bg-white rounded-full p-2 cursor-pointer z-50"
-              onclick={selectPreviousProject}
-            >
-              <FaSolidArrowLeft />
-            </button>
-          </Show>
-        </div>
+        </Show>
+        <Show when={currentIndex() !== 0}>
+          <button
+            id="next-button"
+            class="absolute left-[24px] top-1/2 translate-y-1/2 bg-white rounded-full p-2 cursor-pointer z-50"
+            onclick={selectPreviousProject}
+          >
+            <FaSolidArrowLeft />
+          </button>
+        </Show>
       </Show>
       <div class="text-[18px] leading-[24px] font-bold mt-[16px] mb-[24px] px-[20px]">Projects</div>
       <div class="px-[20px] w-[935px] grid grid-cols-3 gap-[28px]">
         <For each={projectList()}>{(project, i) =>
-          <img class="h-[293px] w-[293px] object-cover"
+          <img class="h-[293px] w-[293px] object-cover cursor-pointer"
             src={project.media[0]}
             onclick={() => selectProject(i())}
           />
