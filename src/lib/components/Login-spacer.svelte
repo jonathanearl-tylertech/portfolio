b5dc2a0df1a874c1677029367272642a15ee1b0d
jonathan earl <earl.jonathan@gmail.com>
2023-02-04 15:34:16 -0800

    clean up

--- src/lib/components/Login-spacer.svelte
+++
@@ -1,5 +0,0 @@
-	<div class="flex items-center text-slate-400 font-semibold py-[28px]">
-    <span class="flex grow h-[1px] bg-slate-400" />
-    <span class="uppercase p-2">or</span>
-    <span class="flex grow h-[1px] bg-slate-400" />
-  </div> 
--- src/routes/logout/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = ({ cookies }) => {
-  cookies.delete('sid');
-  throw redirect(302, '/login');
-};
